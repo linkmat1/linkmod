932ed2095b04ffa449c3d6806e181bdd44ed49ed
Mathieu Gendron <priscusio@gmail.com>
2020-03-02 06:56:24 -0500

    Ajout du TwigIcon

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,66 @@
-const Encore = require('@symfony/webpack-encore')
-const path = require('path')
+let Encore = require('@symfony/webpack-encore')
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
 }
 
 Encore
-  .setOutputPath('public/assets/')
-  .setPublicPath('/assets')
-    // only needed for CDN's or sub-directory deploy
-    // .setManifestKeyPrefix('assets/')
-    .addEntry('app', './assets/js/app.js')
-    .addEntry('admin', './assets/js/admin.js')
-    .splitEntryChunks()
-    .enableSingleRuntimeChunk()
-    .configureBabel(c => { return {} })
-    .addAliases({
-        svelte: path.resolve('node_modules', 'svelte'),
-        '@fn': path.resolve('assets', 'js', 'functions'),
-        '@el': path.resolve('assets', 'js', 'elements')
-    })
-    .addLoader(
-        {
-            test: /\.(html|svelte)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'svelte-loader',
-            }}
-    )
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
-    .cleanupOutputBeforeBuild()
-    // .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+  // directory where compiled assets will be stored
+  .setOutputPath('public/build/')
+  // public path used by the web server to access the output path
+  .setPublicPath('/build')
+  // only needed for CDN's or sub-directory deploy
+  //.setManifestKeyPrefix('build/')
 
-    // enables Sass/SCSS support
-    .enableSassLoader()
+  /*
+   * ENTRY CONFIG
+   *
+   * Add 1 entry for each "page" of your app
+   * (including one that's included on every page - e.g. "app")
+   *
+   * Each entry will result in one JavaScript file (e.g. app.js)
+   * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+   */
+  .addEntry('app', './assets/js/app.js')
+  //.addEntry('page1', './assets/js/page1.js')
+  //.addEntry('page2', './assets/js/page2.js')
 
-// uncomment if you use TypeScript
-// .enableTypeScriptLoader()
+  // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+  .splitEntryChunks()
 
-// uncomment to get integrity="..." attributes on your script & link tags
-// requires WebpackEncoreBundle 1.4 or higher
-//.enableIntegrityHashes(Encore.isProduction())
+  // will require an extra script tag for runtime.js
+  // but, you probably want this, unless you're building a single-page app
+  .enableSingleRuntimeChunk()
+
+  /*
+   * FEATURE CONFIG
+   *
+   * Enable & configure other features below. For a full
+   * list of features, see:
+   * https://symfony.com/doc/current/frontend.html#adding-more-features
+   */
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  // enables hashed filenames (e.g. app.abc123.css)
+  .enableVersioning(Encore.isProduction())
+
+  // enables @babel/preset-env polyfills
+  .configureBabel(c => { return {} })
+  .configureLoaderRule('eslint', loaderRule => {
+    loaderRule.test = /\.(jsx?|vue)$/
+  })
+  // enables Sass/SCSS support
+  .enableSassLoader()
+
+  // uncomment if you use TypeScript
+  //.enableTypeScriptLoader()
+
+  // uncomment to get integrity="..." attributes on your script & link tags
+  // requires WebpackEncoreBundle 1.4 or higher
+  .enableIntegrityHashes(Encore.isProduction())
 
 // uncomment if you're having problems with a jQuery plugin
 //.autoProvidejQuery()
@@ -59,11 +68,6 @@ Encore
 // uncomment if you use API Platform Admin (composer req api-admin)
 //.enableReactPreset()
 //.addEntry('admin', './assets/js/admin.js')
+;
 
-if (!Encore.isProduction()) {
-    Encore.disableCssExtraction()
-}
-
-const config = Encore.getWebpackConfig()
-config.resolve.extensions.push('.svelte')
-config.resolve.mainFields = ['svelte', 'browser', 'module', 'main']
+module.exports = Encore.getWebpackConfig()
