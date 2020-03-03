4e662ad2f15fcb5aa1b9185d11221c5d690c73da
Mathieu Gendron <priscusio@gmail.com>
2020-03-02 20:22:03 -0500

    Ajout du Systeme de blog

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param PostRepository $posts
+     * @return Response
      */
-    public function index()
+    public function index(PostRepository $posts): Response
     {
-        return $this->render('home/index.html.twig', [
+        $rows =  $posts->findAll();
 
+        return $this->render('home/index.html.twig', [
+            'rows' => $rows
         ]);
     }
 }
