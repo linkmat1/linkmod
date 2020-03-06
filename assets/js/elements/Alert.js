7489cacce9cf2651d32edd9177209f91742e6d79
Mathieu Gendron <priscusio@gmail.com>
2020-03-05 21:23:36 -0500

    filtre repository public | + Début de la home page

---
+++ assets/js/elements/Alert.js
@@ -0,0 +1,59 @@
+/**
+ * @property {string} type
+ */
+import {slideUp} from '../modules/animation'
+
+export default class Alert extends global.HTMLElement  {
+  constructor ({type, message} = {}) {
+    super()
+    if (type !== undefined) {
+      this.type = type
+    } else {
+      this.type = this.getAttribute('type')
+    }
+    if (!message) {
+      message = this.innerHTML
+    }
+    if (this.type === 'error' || this.type === null) {
+      this.type = 'danger'
+    }
+    this.message = message
+  }
+
+  connectedCallback () {
+    this.innerHTML = `<div class="alert alert-${this.type}">
+        <svg class="icon icon-{$name}">
+          <use xlink:href="/sprite.svg#${this.icon}"></use>
+        </svg>
+        ${this.message}
+        <button class="alert-close">
+          <svg class="icon">
+            <use xlink:href="/sprite.svg#cross"></use>
+          </svg>
+        </button>
+      </div>`
+    this.querySelector('.alert-close').addEventListener('click', (e) => {
+      e.preventDefault()
+      this.close()
+    })
+  }
+
+  close () {
+    const element = this.querySelector('.alert')
+    element.classList.add('out')
+    window.setTimeout(async () => {
+      await slideUp(element)
+      this.parentElement.removeChild(this)
+    }, 500)
+  }
+
+  get icon () {
+    if (this.type === 'danger') {
+      return 'warning'
+    } else if (this.type === 'success') {
+      return 'check'
+    }
+  }
+}
+
+global.customElements.define('alert-message', Alert)
---
+++ assets/js/libs/turbolinks.js
@@ -0,0 +1,14 @@
+import Turbolinks from 'turbolinks'
+
+
+document.addEventListener('turbolinks:load', function () {
+  const darkToggle = document.querySelector('#dark-toggle')
+  if (darkToggle) {
+    darkToggle.addEventListener('click', e => {
+      e.stopPropagation()
+      e.preventDefault()
+      document.body.classList.toggle('dark')
+    })
+  }
+})
+Turbolinks.start()
