9c8e6dd7f6efb4bb20751c995639fdc71aa9e895
Mathieu <priscusio@gmail.com>
2020-03-09 03:18:44 -0400

    Ajout de l'entity Mods

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
